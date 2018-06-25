460a609b60c04efdfeedd5d2755eaf943430177f
liushunyun <liushunyun@58ganji.com>
2018-06-25 20:14:11 +0800

    first pkg test

---
+++ pkgtest.js
@@ -0,0 +1,10 @@
+/**
+ * pkgtestpkg - description
+ *
+ * @param  {type} name description
+ * @return {type}      description
+ */
+function pkgtestpkg(name) {
+    console.log('hello ',name||'default name');
+}
+exports.pkgtestpkg = pkgtestpkg;
