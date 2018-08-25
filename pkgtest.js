cfd5fb6b9e3ccf626d3c1f4bd8f3bdd922bd4c05
风云 <fengyun@nooboy.local>
2018-08-25 18:52:30 +0800

    v2

--- pkgtest.js
+++ pkgtest.js
@@ -4,7 +4,17 @@
  * @param  {type} name description
  * @return {type}      description
  */
-function pkgtestpkg(name) {
-    console.log('hello ',name||'default name');
+// function pkgtestpkg(name) {
+//     console.log('hello ',name||'default name');
+// }
+// exports.default = pkgtestpkg;
+
+module.exports = {
+    sum: function(a, b) {
+        return a+b
+    },
+    sub: function(a, b) {
+        return a-b
+    }
 }
-exports.pkgtestpkg = pkgtestpkg;
+
